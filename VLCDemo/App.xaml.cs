9966d00bd9c38606d71f60919cae7e7097a83425
Sukhesh <sukhesh.majalkar@xtreme-media.in>
2023-07-13 15:43:50 +0530

    Initial Commit

---
+++ VLCDemo/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VLCDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+           string _VlcLibPath =  System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LibVlc", "win-x86");
+            Core.Initialize(_VlcLibPath);
+        }
+    }
+}
---
+++ VLCDemo/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using LibVLCSharp.Shared;
+using LibVLCSharp.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VLCDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public LibVLC _libVLC = null;
+        LibVLCSharp.Shared.MediaPlayer _mediaPlayer;
+        string filePath = string.Empty;
+        Timer _timer = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Videos", "1.mp4");
+        }
+
+        private void VideoView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _libVLC = new LibVLC(enableDebugLogs: true);
+            _mediaPlayer = new LibVLCSharp.Shared.MediaPlayer(_libVLC);
+            _mediaPlayer.EnableHardwareDecoding = true;
+
+            VideoView.MediaPlayer = _mediaPlayer;
+            PlayVideo(filePath);
+            if (_timer == null)
+            {
+                _timer = new Timer();
+                _timer.Interval = 10*1000;
+                _timer.Elapsed += _timer_Elapsed;
+                _timer.Start();
+            }
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                if (VideoView.MediaPlayer.IsPlaying && VideoView.MediaPlayer.IsSeekable)
+                {
+                    VideoView.MediaPlayer.Time = 0;
+                }
+                else
+                {
+                    PlayVideo(filePath);
+                }
+            });
+        }
+
+        private void PlayVideo(string Path)
+        {
+            using (var media = new LibVLCSharp.Shared.Media(_libVLC, new Uri(Path)))
+            {
+                media.AddOption(new MediaConfiguration { EnableHardwareDecoding = true, FileCaching = 300, NetworkCaching = 300 });
+                VideoView?.MediaPlayer?.Play(media);
+            }
+        }
+
+    }
+}
